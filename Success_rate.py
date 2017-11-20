de403adcbff88e4f5bfa9b2856ea4f603e3af37d
cezzo39 <cmutlu93@gmail.com>
2017-11-20 12:20:48 -0800

    Update Success_rate.py

--- Success_rate.py
+++ Success_rate.py
@@ -5,6 +5,9 @@ data=data[data["NUM_OF_APPS"]>3]
 lookup = data.EMPLOYER_NAME.value_counts()
 lookup2 = dict(zip(data.EMPLOYER_NAME.unique(),np.zeros((data.EMPLOYER_NAME.value_counts().size))))
 lkp=lookup.size
+dd=data.copy()
 for i in range(0,lkp):
-    lookup2[lookup.index[i]]=(np.sum(data[data.EMPLOYER_NAME==lookup.index[i]].CASE_STATUS.values))/lookup[i]
+    loc=dd.EMPLOYER_NAME.values==lookup.index[i]
+    lookup2[lookup.index[i]]=(np.sum(dd[loc].CASE_STATUS.values))/lookup[i]
+    dd=dd[np.invert(loc)]
 data['SUCCESS_RATE']= data['NUM_OF_APPS'].map(lookup2)
